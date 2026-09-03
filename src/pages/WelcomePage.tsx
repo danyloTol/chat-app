69f533e25a84e17b38b9f75e20f598b0f8327970
danyloTol <tolochkodanylo.dev@gmail.com>
2026-09-04 00:11:25 +0300

    feat: add 2 buttons (Get Started & Log In) in welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,8 +1,12 @@
+import Button from '../components/Button';
+
 function WelcomePage() {
     return (
         <div>
             <h1>WELCOME</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <Button text='Get Started'/>
+            <Button text='Log In'/>
         </div>
     )
 }
