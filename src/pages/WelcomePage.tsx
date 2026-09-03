a461983f52aaf12f534d53b7eabcacf987823264
danyloTol <tolochkodanylo.dev@gmail.com>
2026-09-04 02:05:36 +0300

    style: add fade in animation

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -5,10 +5,10 @@ function WelcomePage() {
 
     return (
         <div className='w-full h-full flex flex-col items-center'>
-            <h1 className='text-8xl font-bold mt-15'>WELCOME</h1>
-            <p className='text-xl mb-70'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p className='text-xl my-5'>Do you wanna join us?</p>
-            <div className='flex flex-row gap-10'>
+            <h1 className='text-8xl font-bold mt-15 fade-in-element-1s'>WELCOME</h1>
+            <p className='text-xl mb-70 fade-in-element-2s'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p className='text-xl my-5 fade-in-element-2s'>Do you wanna join us?</p>
+            <div className='flex flex-row gap-10 fade-in-element-3s'>
                 <Button text='Get Started' type='default' color='#000000' padY={10} width={BUTTON_WIDTH} fontSize={24}/>
                 <Button text='Log In' type='outline' color='#000000' padY={10} width={BUTTON_WIDTH} fontSize={24}/>
             </div>
