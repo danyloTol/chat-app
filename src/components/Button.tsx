3f6af01ae763a9b99bbb4c6c8e75e6b038c05d3b
danyloTol <tolochkodanylo.dev@gmail.com>
2026-09-05 19:26:39 +0300

    style: add hover color change in button component & change layout styles in welcome page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 type ButtonProps = {
     text: string;
-    color?: string;
+    bgColor?: string;
+    textColor?: string;
+    hoverColor?: string;
     type?: "default" | "outline";
     padX?: number;
     padY?: number;
@@ -8,15 +12,19 @@ type ButtonProps = {
     fontSize?: number;
 }
 
-function Button({text, color = "#FFFFFF", type = "default", padX = 0, padY = 0, width, fontSize = 16}: ButtonProps) {
+function Button({text, bgColor = "#FFFFFF", hoverColor, type = "default", padX = 0, padY = 0, width, fontSize = 16}: ButtonProps) {
+    const [isHovered, setIsHovered] = useState(false)
     const isDefault = type === "default";
 
     return (
         <button 
-            className={`rounded-full px-[${padX}] py-[${padY}] cursor-pointer ${isDefault ? "text-white" : "bg-transparent trxt-black"}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className={`rounded-full px-[${padX}] py-[${padY}] cursor-pointer ${isDefault ? "text-white" : "bg-transparent trxt-black"} duration-300`}
             style={{
-                backgroundColor: isDefault ? color : undefined,
-                outline: !isDefault ? `2px solid ${color}`: undefined,
+                backgroundColor: isDefault ? isHovered ? hoverColor : bgColor : isHovered ? hoverColor : undefined,  
+                color: isDefault ? '#FFFFFF' : bgColor,
+                border: !isDefault ? `3px solid ${bgColor}`: undefined,
                 padding: `${padY}px ${padX}px`, 
                 width: width,
                 fontSize: fontSize,
