bbdf9c4de8c359d18c8b52513745f3aa4b672a03
danyloTol <tolochkodanylo.dev@gmail.com>
2026-09-10 22:41:51 +0300

    style: replace show/hide button with visiblity icon

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -68,7 +68,14 @@ export default function LoginPage() {
                         onClick={() => setShowPassword(!showPassword)}
                         className='absolute right-3 top-1/2 -translate-y-1/2 text-sm text-gray-500 hover:text-gray-700 select-none'
                     >
-                        {showPassword ? "Hide" : "Show"}
+                        {showPassword ? (
+                            <span className="material-symbols-outlined">
+                                visibility
+                            </span>
+                        ) : ( <span className="material-symbols-outlined">
+                                visibility_off
+                            </span>
+                        )}
                     </button>
                 </div>
                 
